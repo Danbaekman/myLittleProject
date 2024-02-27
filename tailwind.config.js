69464b0688ed93a938639ea87afc8846359b8758
mjmj0217 <mjmj0217@naver.com>
2024-02-27 12:59:17 +0900

    Feat: theme colors 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        crOrange: "#ff6f0f",
+        crGreen: "#02a15c",
+        crYellow: "#fffae0",
+        crWhite: "#ffffff",
+        crBlack: "#000000",
+      },
+    },
   },
   plugins: [],
 };
